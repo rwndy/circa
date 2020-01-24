ad919a3aea60a17ddd05b377bcfecf52c87249b8
rwndy <riwandy10@gmail.com>
2020-01-25 00:26:56 +0700

    adding smooth scrolling

--- js/index.js
+++ js/index.js
@@ -10,3 +10,11 @@ $(document).ready(function(){
     $("html, body").animate({scrollTop: $("#nav").offset().top}, 500);
   });
 });
+
+$(document).on('click', 'a[href^="#"]', function (event) {
+  event.preventDefault();
+
+  $('html, body').animate({
+      scrollTop: $($.attr(this, 'href')).offset().top
+  }, 500);
+});
